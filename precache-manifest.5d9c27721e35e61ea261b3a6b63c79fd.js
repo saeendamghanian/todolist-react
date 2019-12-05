5231e7c148feafe82879ffed4f52063b820a3ef3
Saeen-Damghanian <saeen.damghanian@gmail.com>
2019-12-05 17:19:12 +0100

    Updates

---
+++ precache-manifest.5d9c27721e35e61ea261b3a6b63c79fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce637d150c1792f93f20a3bf0846ed6d",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "579ce41d6794bd5eacf1",
+    "url": "/todolist-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "843556cd9eaf75c4f2ab",
+    "url": "/todolist-react/static/js/2.46dd68d8.chunk.js"
+  },
+  {
+    "revision": "579ce41d6794bd5eacf1",
+    "url": "/todolist-react/static/js/main.a21b1549.chunk.js"
+  },
+  {
+    "revision": "60f9fa365c1ea7265e45",
+    "url": "/todolist-react/static/js/runtime-main.80949acb.js"
+  }
+]);
